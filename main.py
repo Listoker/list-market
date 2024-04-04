5a04004fda36029452ac6554a7d6217e2b1c31d9
Listoker <151549199+Listoker@users.noreply.github.com>
2024-04-04 17:54:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -213,6 +213,37 @@ def listm():
 </html>'''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def cosmos(nickname, level, rating):
+    level = str(level)
+    rating = str(rating)
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Результаты</title>
+                  </head>
+                  <body>
+                    <h1>Результаты отбора</h1>
+                    <h2>Претендент на участие в миссии {nickname}:</h2>
+                    <div class="alert alert-primary" role="alert">
+                      Поздравляем! Ваш рейтинг после {level} этапа отбора
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      составляет {rating}!
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      Желаем удачи!
+                    </div>
+                  </body>
+                </html>'''
+
+
 def human_read_format(size):
     values = ['Б', "КБ", "МБ", "ГБ"]
     num = 0
