a1b15f45404b3f2c2885184af89bfdf1e64471f5
Listoker <151549199+Listoker@users.noreply.github.com>
2024-04-12 18:28:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ from data.users import User
 import datetime
 from data.news import News
 from forms.user import RegisterForm
+from flask import Flask, redirect, render_template
+from data import db_session
+from data.users import User
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 def main():
@@ -28,11 +41,61 @@ def news():
         return render_template('css/index.html', name=name, foto=foto, x=x)
 
 
+@app.route('/list-market_')
+def newss():
+    with open('data_market/info_tovar.txt') as f:
+        tovari = f.read().split('#')
+        x = 0
+        name = []
+        foto = []
+        for i in tovari:
+            ii = i.split()
+            name.append(ii[0])
+            foto.append(ii[1])
+        return render_template('css/index_reg.html', name=name, foto=foto, x=x)
+
+
+@app.route('/list-market_tovar')
+def tovar():
+    with open('data_market/info_tovar.txt') as f:
+        tovari = f.read().split('#')
+        x = 0
+        name = []
+        foto = []
+        for i in tovari:
+            ii = i.split()
+            name.append(ii[0])
+            foto.append(ii[1])
+        return render_template('css/tovar.html', name=name, foto=foto, x=x)
+
+
+@app.route('/list-market_akkaynt')
+def akkaynt():
+    with open('data_market/info_tovar.txt') as f:
+        tovari = f.read().split('#')
+        x = 0
+        name = []
+        foto = []
+        for i in tovari:
+            ii = i.split()
+            name.append(ii[0])
+            foto.append(ii[1])
+        return render_template('css/akkaynt.html', name=name, foto=foto, x=x)
+
+
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
     news = db_sess.query(News).filter(News.is_private != True)
     return render_template("css/register.html", news=news)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/list-market_')
+    return render_template('css/login.html', title='Авторизация', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
